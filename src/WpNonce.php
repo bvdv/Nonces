243c9df27becf581c3f631fadfcda43f7a5f3f3a
bvdv <bvdv2019@gmail.com>
2018-07-27 21:02:54 +0300

    WpNonce.php refactoring

--- src/WpNonce.php
+++ src/WpNonce.php
@@ -12,32 +12,23 @@ class WpNonce extends NonceAbstract
     /**
      * Wp Nonce constructor.
      *
-     * @param $paramAction
+     * @param string $paramAction
      * @param string $paramName
      */
     public function __construct($paramAction, $paramName = '_wpnonce')
     {
         parent::__construct($paramAction, $paramName);
-    }
 
-    /**
-     * Create nonce string
-     *
-     * @return string
-     */
-    public function createWpNonce(): string
-    {
-        $this->changeNonce(wp_create_nonce($this->action()));
-        return $this->nonce();
+        $this->nonce = wp_create_nonce($this->action());
     }
 
     /**
      * Create url with nonce value parameter
      *
-     * @param $paramActionUrl
-     * @return mixed|null
+     * @param string $paramActionUrl
+     * @return string
      */
-    public function createWpNonceUrl($paramActionUrl)
+    public function createWpNonceUrl($paramActionUrl): string
     {
         $actionUrl = str_replace('&amp;', '&', $paramActionUrl);
 
@@ -61,28 +52,27 @@ class WpNonce extends NonceAbstract
      */
     public function validateRequest(): bool
     {
-        $isValid = false;
+        $isRequestValid = false;
+
         if (isset($_REQUEST[ $this->name() ])) {
             $nonceReceived = sanitize_text_field(wp_unslash($_REQUEST[ $this->name() ]));
             $this->changeNonce($nonceReceived);
-            $isValid = $this->validate();
+            $isRequestValid = $this->validate();
         }
-        return $isValid;
+
+        return $isRequestValid;
     }
 
     /**
      * Validate nonce
      *
-     * @param $paramNonce
+     * @param string $paramNonce
      * @return bool
      */
     public function validateNonce(string $paramNonce): bool
     {
-        $isValid = false;
-
         $this->changeNonce($paramNonce);
-        $isValid = $this->validate();
-        return $isValid;
+        return $this->validate();
     }
 
     /**
@@ -94,12 +84,6 @@ class WpNonce extends NonceAbstract
      */
     public function createNonceField(bool $paramReferer = true, bool $paramEcho = true): string
     {
-        $this->createWpNonce();
-        $name = $this->name();
-        $action = $this->action();
-        $nonce = $this->nonce();
-        $name = esc_attr($name);
-
-        return wp_nonce_field($action, $name, $paramReferer, $paramEcho);
+        return wp_nonce_field($this->action(), $this->name(), $paramReferer, $paramEcho);
     }
 }
